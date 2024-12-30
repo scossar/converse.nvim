fc8f3c3783d634464ff604455c82c1968477f3d8
scossar <scossar3@gmail.com>
2024-12-29 21:34:25 -0800

    use ~= operator

--- lua/converse/init.lua
+++ lua/converse/init.lua
@@ -6,7 +6,7 @@ local python_script_path = plugin_path .. "/python/nvim_conversation_manager.py"
 local highlight_ns = vim.api.nvim_create_namespace("converse_highlight")
 
 local function send_to_python(job_id, message)
-  if not vim.fn.jobwait({ job_id }, 0)[1] == -1 then
+  if vim.fn.jobwait({ job_id }, 0)[1] ~= -1 then
     error("Invalid or terminated job_id")
   end
 
